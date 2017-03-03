9b7c5469a8f9d5d8d8237cc64380022b140e1c25
Administrator <1060409179@qq.com>
2017-03-03 17:21:53 +0800

    first unity test
    第一个unity测试项目

---
+++ Assets/Script/FllowCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FllowCamera : MonoBehaviour {
+	public GameObject player;
+	// Update is called once per frame
+	void Update () {
+		Vector3 player_position = player.GetComponent<Transform>().position;
+		this.GetComponent<Transform> ().position = new Vector3(player_position.x,player_position.y + 8.18f,player_position.z - 9.05f);
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private int i = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		float horiziontal_move = Input.GetAxis ("Horizontal");
+		float vertical_move = Input.GetAxis ("Vertical");
+		this.GetComponent<Rigidbody>().AddForce (new Vector3 (horiziontal_move, 0, vertical_move)*10f);
+	}
+	void OnTriggerEnter( Collider other){
+		Debug.Log ("Trigger Enter");
+		if(other.gameObject.name == "Food"){
+			GameObject.Destroy (other.gameObject);
+			i++;
+			Debug.Log ("Eat a food");
+			if(i == 10){
+				Debug.Log ("Game win!");
+			}
+		}
+	}
+//	void OnTriggerExit(){
+//		Debug.Log ("Trigger Exit");
+//	}
+//	void OnTriggerStay(){
+//		Debug.Log ("Trigger Stay");
+//	}
+}
